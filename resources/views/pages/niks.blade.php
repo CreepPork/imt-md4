c38931493e78b4c4ad967b95774506b4934850d9
Ralfs Garkaklis <ralfs@garkaklis.com>
2023-05-05 12:42:38 +0300

    Add Niks and Sandris pages

---
+++ resources/views/pages/niks.blade.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Frequently asked questions</title>
+    <script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "FAQPage",
+        "mainEntity": [{
+          "@type": "Question",
+          "name": "What are FAQ Rich Snippets?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "FAQ Rich Snippets are a feature provided by Google that displays the most relevant and concise answers to users' frequently asked questions directly in the search results."
+          }
+        }, {
+          "@type": "Question",
+          "name": "How do I enable FAQ Rich Snippets for my website?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "To enable FAQ Rich Snippets for your website, you need to add structured data markup (using JSON-LD, Microdata, or RDFa) to your website's FAQ pages. This helps Google understand and index the content for display in search results."
+          }
+        }, {
+          "@type": "Question",
+          "name": "Can I customize the appearance of my FAQ Rich Snippets?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "While you cannot directly customize the appearance of FAQ Rich Snippets, you can optimize your content and markup to ensure that the most relevant and accurate information is displayed."
+          }
+        }, {
+          "@type": "Question",
+          "name": "How does Google determine which FAQs to display?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": "Google's algorithm considers various factors, such as the relevance of the content, the quality of the website, and the user's search query, to determine which FAQs to display in the search results."
+          }
+        }, {
+          "@type": "Question",
+          "name": "Can I track the performance of my FAQ Rich Snippets?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text":"Yes, you can track the performance of your FAQ Rich Snippets using Google Search Console. This tool provides insights into impressions, clicks, and click-through rates for your snippets."
+          }
+        }, {
+          "@type": "Question",
+          "name": "Are there any guidelines for creating FAQ content for Rich Snippets?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text":"Yes, Google provides guidelines for creating high-quality FAQ content. Some key points include providing clear and concise answers, avoiding promotional content, and ensuring that the content is unique and not duplicated across multiple pages."
+          }
+        },  {
+          "@type": "Question",
+          "name": "Can FAQ Rich Snippets impact my website's SEO?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text":"Yes, FAQ Rich Snippets can positively impact your website's SEO by providing users with quick and relevant answers, which can lead to increased click-through rates and user engagement."
+          }
+        },  {
+          "@type": "Question",
+          "name": "Can I use FAQ Rich Snippets for multiple languages?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text":"Yes, you can use FAQ Rich Snippets for multiple languages by implementing the appropriate hreflang tags and structured data markup for each language version of your FAQ pages."
+          }
+        },  {
+          "@type": "Question",
+          "name": "What if my FAQ Rich Snippets are not showing up in search results?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text":"If your FAQ Rich Snippets are not showing up in search results, ensure that your structured data markup is correctly implemented and follows Google's guidelines. You can also use Google's Rich Results Test tool to validate your markup."
+          }
+        },  {
+          "@type": "Question",
+          "name": "Can I report an issue or provide feedback on FAQ Rich Snippets?",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text":"Yes, you can report issues or provide feedback on FAQ Rich Snippets using the 'Send feedback' option available at the bottom of the search results page or through Google Search Console."
+          }
+        }]
+      }
+    </script>
+</head>
+
+<body>
+    <h1>FAQ Rich Snippets</h1>
+    <div id="faq">
+        <details>
+            <summary>What are FAQ Rich Snippets?</summary>
+            <p>
+                FAQ Rich Snippets are a feature provided by Google that displays the
+                most relevant and concise answers to users' frequently asked questions
+                directly in the search results.
+            </p>
+        </details>
+        <details>
+            <summary>How do I enable FAQ Rich Snippets for my website?</summary>
+            <p>
+                To enable FAQ Rich Snippets for your website, you need to add
+                structured data markup (using JSON-LD, Microdata, or RDFa) to your
+                website's FAQ pages. This helps Google understand and index the
+                content for display in search results.
+            </p>
+        </details>
+        <details>
+            <summary>
+                Can I customize the appearance of my FAQ Rich Snippets?
+            </summary>
+            <p>
+                While you cannot directly customize the appearance of FAQ Rich
+                Snippets, you can optimize your content and markup to ensure that the
+                most relevant and accurate information is displayed.
+            </p>
+        </details>
+        <details>
+            <summary>How does Google determine which FAQs to display?</summary>
+            <p>
+                Google's algorithm considers various factors, such as the relevance of
+                the content, the quality of the website, and the user's search query,
+                to determine which FAQs to display in the search results.
+            </p>
+        </details>
+        <details>
+            <summary>Can I track the performance of my FAQ Rich Snippets?</summary>
+            <p>
+                Yes, you can track the performance of your FAQ Rich Snippets using
+                Google Search Console. This tool provides insights into impressions,
+                clicks, and click-through rates for your snippets.
+            </p>
+        </details>
+        <details>
+            <summary>
+                Are there any guidelines for creating FAQ content for Rich Snippets?
+            </summary>
+            <p>
+                Yes, Google provides guidelines for creating high-quality FAQ content.
+                Some key points include providing clear and concise answers, avoiding
+                promotional content, and ensuring that the content is unique and not
+                duplicated across multiple pages.
+            </p>
+        </details>
+        <details>
+            <summary>Can FAQ Rich Snippets impact my website's SEO?</summary>
+            <p>
+                Yes, FAQ Rich Snippets can positively impact your website's SEO by
+                providing users with quick and relevant answers, which can lead to
+                increased click-through rates and user engagement.
+            </p>
+        </details>
+        <details>
+            <summary>Can I use FAQ Rich Snippets for multiple languages?</summary>
+            <p>
+                Yes, you can use FAQ Rich Snippets for multiple languages by
+                implementing the appropriate hreflang tags and structured data markup
+                for each language version of your FAQ pages.
+            </p>
+        </details>
+        <details>
+            <summary>
+                What if my FAQ Rich Snippets are not showing up in search results?
+            </summary>
+            <p>
+                If your FAQ Rich Snippets are not showing up in search results, ensure
+                that your structured data markup is correctly implemented and follows
+                Google's guidelines. You can also use Google's Rich Results Test tool
+                to validate your markup.
+            </p>
+        </details>
+        <details>
+            <summary>
+                Can I report an issue or provide feedback on FAQ Rich Snippets?
+            </summary>
+            <p>
+                Yes, you can report issues or provide feedback on FAQ Rich Snippets
+                using the "Send feedback" option available at the bottom of the search
+                results page or through Google Search Console.
+            </p>
+        </details>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/pages/sandris.blade.php
@@ -0,0 +1,111 @@
+<html>
+
+<head>
+    <title>How to build first PC</title>
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "HowTo",
+      "image": {
+        "@type": "ImageObject",
+        "url": "https://blog.bestbuy.ca/wp-content/uploads/2020/06/build-pc-man-tools-16x9.jpg.rendition.intel_.web_.1072.603.jpg"
+      },
+      "name": "How to build first PC",
+      "datePublished": "2023-06-04",
+      "author": {
+        "@type": "Person",
+        "name": "Sandris Safronovs"
+      },
+      "supply": [
+        {
+          "@type": "HowToSupply",
+          "name": "Motherboard"
+        }, {
+          "@type": "HowToSupply",
+          "name": "CPU"
+        }, {
+          "@type": "HowToSupply",
+          "name": "GPU"
+        }, {
+          "@type": "HowToSupply",
+          "name": "RAM"
+        }, {
+          "@type": "HowToSupply",
+          "name": "PSU"
+        }, {
+          "@type": "HowToSupply",
+          "name": "Case"
+        }, {
+          "@type": "HowToSupply",
+          "name": "Cooling"
+        }
+      ],
+      "tool": [
+        {
+          "@type": "HowToTool",
+          "name": "Screwdriver"
+        }
+      ],
+      "step":[
+        {
+          "@type": "HowToStep",
+          "name": "Preparation",
+          "text": "To start building your first computer, it is necessary to understand what tasks it will perform. The choice of components will depend on it.",
+          "image": "https://imgur.com/IQLt3Qz.jpg",
+        }, {
+          "@type": "HowToStep",
+          "name": "Purchase of compatible parts",
+          "text": "One of the most important steps is purchasing compatible parts. Depending on the task assigned to the computer, check whether the component will be compatible with the others before you buy anything.",
+          "image": "https://cdn.mos.cms.futurecdn.net/ayqMjVNaxDUHrvSz2JqNwb-970-80.jpg",
+        }, {
+          "@type": "HowToStep",
+          "name": "Computer assembly",
+          "text": "An undeniably important step. Once compatible parts have been purchased, they must be connected and properly connected. Virtually all components are connected to the motherboard, which is located inside the computer case.",
+          "image": "https://uk.crucial.com/content/dam/crucial/dram-products/desktop/ddr5/images/install/crucial-ddr5-desktop-motherboard-hand-non-blue-image.psd.transform/small-jpg/img.jpg",
+        }, {
+          "@type": "HowToStep",
+          "name": "Checking all connections before starting PC",
+          "text": "When building a computer for the first time, there are bound to be imperfections. Do not be lazy and check all the connections to make sure they are properly connected.",
+          "image": "https://i.insider.com/603fed2d2dc901001835537f?width=1000&format=jpeg",
+        }, {
+          "@type": "HowToStep",
+          "name": "Start the computer, making sure that it doesn't make funny noises or smoke.",
+          "text": "If the components are successfully connected, the computer will start and then it will be necessary to install the original configuration of the computer, as well as the operating system.",
+          "image": "https://img-9gag-fun.9cache.com/photo/abgBW1O_700bwp.webp",
+        }
+      ]
+    }
+    </script>
+</head>
+
+<body>
+    <h1>How to build first PC</h1>
+    <h2>Preparation</h2>
+    <p>To start building your first computer, it is necessary to understand what tasks it will perform. The choice of
+        components will depend on it.</p>
+    <img src="https://imgur.com/IQLt3Qz.jpg">
+
+    <h2>Purchase of compatible parts</h2>
+    <p>One of the most important steps is purchasing compatible parts. Depending on the task assigned to the computer,
+        check whether the component will be compatible with the others before you buy anything.</p>
+    <img src="https://cdn.mos.cms.futurecdn.net/ayqMjVNaxDUHrvSz2JqNwb-970-80.jpg">
+
+    <h2>Computer assembly</h2>
+    <p>An undeniably important step. Once compatible parts have been purchased, they must be connected and properly
+        connected. Virtually all components are connected to the motherboard, which is located inside the computer case.
+    </p>
+    <img
+        src="https://uk.crucial.com/content/dam/crucial/dram-products/desktop/ddr5/images/install/crucial-ddr5-desktop-motherboard-hand-non-blue-image.psd.transform/small-jpg/img.jpg">
+
+    <h2>Checking all connections before starting PC</h2>
+    <p>When building a computer for the first time, there are bound to be imperfections. Do not be lazy and check all
+        the connections to make sure they are properly connected.</p>
+    <img src="https://i.insider.com/603fed2d2dc901001835537f?width=1000&format=jpeg">
+
+    <h2>Start the computer, making sure that it doesn't make funny noises or smoke.</h2>
+    <p>If the components are successfully connected, the computer will start and then it will be necessary to install
+        the original configuration of the computer, as well as the operating system.</p>
+    <img src="https://img-9gag-fun.9cache.com/photo/abgBW1O_700bwp.webp">
+</body>
+
+</html>
